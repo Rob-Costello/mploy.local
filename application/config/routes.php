68adc904d1afa5e7e9d38d43e4871094a74b1e69
Rob Costello <rob.costello@hotmail.co.uk>
2018-04-26 21:57:12 +0100

    db and routing sorted

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
+$route['posts'] = 'posts/index';
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['(:any)'] = 'pages/view/$1';
--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -5,12 +5,18 @@ class Post_model extends CI_Model{
 
 	public function __construct(){
 
+		$this->load->database();
+	
+	}
+
+
+	public function get_posts($slug = FALSE){
+
 		if($slug === FALSE){
-
-			$query = $this->db-get('posts');
+			$query = $this->db->get('mploy_posts');
 			return $query->result_array();
-
 		}
+		
 		$query = $this->db->get_where('posts',array('slug' => $slug));
 		return $query->row_array();
 	}

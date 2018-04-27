a77154ecf9a28c0107885bb98acc6d4a7311f6a4
Rob <rob@hyperexts-iMac.local>
2018-04-27 15:30:20 +0100

    added theme and login template

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,115 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>AdminLTE 2 | Log in</title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<!-- Bootstrap 3.3.7 -->
+	<link rel="stylesheet" href="<?php echo base_url()."assets/";?>bower_components/bootstrap/dist/css/bootstrap.min.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php echo base_url()."assets/";?>bower_components/font-awesome/css/font-awesome.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="<?php echo base_url()."assets/";?>bower_components/Ionicons/css/ionicons.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="<?php echo base_url()."assets/";?>dist/css/AdminLTE.min.css">
+	<!-- iCheck -->
+	<link rel="stylesheet" href="<?php echo base_url()."assets/";?>plugins/iCheck/square/blue.css">
 
-<div id="infoMessage"><?php echo $message;?></div>
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
 
-<?php echo form_open("auth/login");?>
-
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+	<!-- Google Font -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+	<div class="login-logo">
+		<a href="<?php echo base_url()."assets/";?>../../index2.html"><b>Mploy</b></a>
+	</div>
+	<!-- /.login-logo -->
+	<div class="login-box-body">
+		<p class="login-box-msg">Sign in to start your session</p>
 
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+		</p>
 
-<?php echo form_close();?>
+		<p>
+			<?php echo lang('login_password_label', 'password');?>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+
+
+		<form action="<?php echo base_url() ?>auth/login" method="post">
+			<div class="form-group has-feedback">
+				<!--<input type="email" class="form-control" placeholder="Email">-->
+				<?php
+				$identity['class']='form-control';
+				$identity['placeholder'] = 'Email';
+				echo form_input($identity);?>
+
+				<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+			</div>
+			<div class="form-group has-feedback">
+				<!--<input type="password" class="form-control" placeholder="Password">-->
+				<?php
+				$password['class']='form-control';
+				$password['placeholder'] = 'Password';
+				echo form_input($password);?>
+				<span class="glyphicon -lock form-control-feedback"></span>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-8">
+					<div class="checkbox icheck">
+						<label>
+							<input type="checkbox"> Remember Me
+						</label>
+					</div>
+				</div>
+				<!-- /.col -->
+				<div class="col-xs-4">
+					<button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+				</div>
+				<!-- /.col -->
+			</div>
+		</form>
+
+		<!--<div class="social-auth-links text-center">
+			<p>- OR -</p>
+			<a href="<?php echo base_url()."assets/";?>#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+				Facebook</a>
+			<a href="<?php echo base_url()."assets/";?>#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+				Google+</a>
+		</div>
+		<!-- /.social-auth-links -->
+
+		<a href="<?php echo base_url()."assets/";?>#">I forgot my password</a><br>
+		<a href="<?php echo base_url()."assets/";?>register.html" class="text-center">Register a new membership</a>
+
+	</div>
+	<!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+<script src="<?php echo base_url()."assets/";?>bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url()."assets/";?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url()."assets/";?>plugins/iCheck/icheck.min.js"></script>
+<script>
+	$(function () {
+		$('input').iCheck({
+			checkboxClass: 'icheckbox_square-blue',
+			radioClass: 'iradio_square-blue',
+			increaseArea: '20%' /* optional */
+		});
+	});
+</script>
+</body>
+</html>

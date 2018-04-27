0f230dc67a582e94efa472cfdc56fc6572b41071
Rob <rob@hyperexts-iMac.local>
2018-04-27 09:02:48 +0100

    added admin LTE theme and configured in CI

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['auth'] = 'auth';
 $route['posts'] = 'posts/index';
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -15,6 +15,7 @@ Class Pages extends CI_Controller{
 	$this->load->view('templates/header');
 	$this->load->view('pages/'.$page, $data);
 	$this->load->view('templates/footer');
+
 	}
 
 

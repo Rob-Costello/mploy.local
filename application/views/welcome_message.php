76b311e63857b80e5d22b857517ee7f69c3489ac
Rob Costello <rob.costello@hotmail.co.uk>
2018-04-29 21:42:02 +0100

    added top toolbar components to theme

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -33,8 +33,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<script src="<?php echo base_url()."assets/";?>https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="<?php echo base_url()."assets/";?>https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 
 	<!-- Google Font -->
@@ -61,82 +61,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="navbar-custom-menu">
 				<ul class="nav navbar-nav">
 					<!-- Messages: style can be found in dropdown.less-->
-					<li class="dropdown messages-menu">
-						<a href="<?php echo base_url()."assets/";?>#" class="dropdown-toggle" data-toggle="dropdown">
-							<i class="fa fa-envelope-o"></i>
-							<span class="label label-success">4</span>
-						</a>
-						<ul class="dropdown-menu">
-							<li class="header">You have 4 messages</li>
-							<li>
-								<!-- inner menu: contains the actual data -->
-								<ul class="menu">
-									<li><!-- start message -->
-										<a href="<?php echo base_url()."assets/";?>#">
-											<div class="pull-left">
-												<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-												Support Team
-												<small><i class="fa fa-clock-o"></i> 5 mins</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<!-- end message -->
-									<li>
-										<a href="<?php echo base_url()."assets/";?>#">
-											<div class="pull-left">
-												<img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-												AdminLTE Design Team
-												<small><i class="fa fa-clock-o"></i> 2 hours</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<li>
-										<a href="<?php echo base_url()."assets/";?>#">
-											<div class="pull-left">
-												<img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-												Developers
-												<small><i class="fa fa-clock-o"></i> Today</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<li>
-										<a href="<?php echo base_url()."assets/";?>#">
-											<div class="pull-left">
-												<img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-												Sales Department
-												<small><i class="fa fa-clock-o"></i> Yesterday</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-									<li>
-										<a href="<?php echo base_url()."assets/";?>#">
-											<div class="pull-left">
-												<img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
-											</div>
-											<h4>
-												Reviewers
-												<small><i class="fa fa-clock-o"></i> 2 days</small>
-											</h4>
-											<p>Why not buy a new awesome theme?</p>
-										</a>
-									</li>
-								</ul>
-							</li>
-							<li class="footer"><a href="<?php echo base_url()."assets/";?>#">See All Messages</a></li>
-						</ul>
-					</li>
+
 					<!-- Notifications: style can be found in dropdown.less -->
 					<li class="dropdown notifications-menu">
 						<a href="<?php echo base_url()."assets/";?>#" class="dropdown-toggle" data-toggle="dropdown">
@@ -260,13 +185,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<!-- User Account: style can be found in dropdown.less -->
 					<li class="dropdown user user-menu">
 						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							<img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+							<img src="<?php echo base_url()."assets/";?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
 							<span class="hidden-xs">Alexander Pierce</span>
 						</a>
 						<ul class="dropdown-menu">
 							<!-- User image -->
 							<li class="user-header">
-								<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+								<img src="<?php echo base_url()."assets/";?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
 								<p>
 									Alexander Pierce - Web Developer
@@ -314,7 +239,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<!-- Sidebar user panel -->
 			<div class="user-panel">
 				<div class="pull-left image">
-					<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+					<img src="<?php echo base_url()."assets/";?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 				</div>
 				<div class="pull-left info">
 					<p>Alexander Pierce</p>
@@ -626,7 +551,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<div class="box-body chat" id="chat-box">
 							<!-- chat item -->
 							<div class="item">
-								<img src="dist/img/user4-128x128.jpg" alt="user image" class="online">
+								<img src="<?php echo base_url()."assets/";?>dist/img/user4-128x128.jpg" alt="user image" class="online">
 
 								<p class="message">
 									<a href="#" class="name">
@@ -653,7 +578,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- /.item -->
 							<!-- chat item -->
 							<div class="item">
-								<img src="dist/img/user3-128x128.jpg" alt="user image" class="offline">
+								<img src="<?php echo base_url()."assets/";?>dist/img/user3-128x128.jpg" alt="user image" class="offline">
 
 								<p class="message">
 									<a href="#" class="name">
@@ -668,7 +593,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- /.item -->
 							<!-- chat item -->
 							<div class="item">
-								<img src="dist/img/user2-160x160.jpg" alt="user image" class="offline">
+								<img src="<?php echo base_url()."assets/";?>dist/img/user2-160x160.jpg" alt="user image" class="offline">
 
 								<p class="message">
 									<a href="#" class="name">
@@ -1233,41 +1158,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/jquery/dist/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
 	$.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
-<script src="bower_components/raphael/raphael.min.js"></script>
-<script src="bower_components/morris.js/morris.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/raphael/raphael.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/morris.js/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
 <!-- jvectormap -->
-<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="<?php echo base_url()."assets/";?>plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="bower_components/moment/min/moment.min.js"></script>
-<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/moment/min/moment.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
-<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo base_url()."assets/";?>bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?php echo base_url()."assets/";?>dist/js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
+<script src="<?php echo base_url()."assets/";?>dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="<?php echo base_url()."assets/";?>dist/js/demo.js"></script>
 </body>
 </html>

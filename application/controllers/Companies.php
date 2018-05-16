4bdb714b3ee0bb1dcdcad63b791609f04331b3ea
Paul Winter <paul@stickymedia.co>
2018-05-16 15:07:39 +0100

    Company Controller - Added same variable to contacts and history.

--- application/controllers/Companies.php
+++ application/controllers/Companies.php
@@ -71,6 +71,7 @@ class Companies extends CI_Controller
     {
         $data['id']=$id;
         $data['page'] = 'contacts';
+        $data['user']=$this->user;
         $company = new CompaniesModel();
 
         $offset=0;
@@ -153,6 +154,7 @@ class Companies extends CI_Controller
 
         $data['id']=$id;
         $data['page'] = 'history';
+        $data['user']=$this->user;
         $company = new CompaniesModel();
 
         $offset=0;

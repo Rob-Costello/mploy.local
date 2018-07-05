8142128123f07c4774cc1aaff26bd6011521a177
hyperext-rob <rob.costello@hotmail.co.uk>
2018-07-05 12:47:58 +0100

    removed limit for dashboard

--- application/models/WexModel.php
+++ application/models/WexModel.php
@@ -15,10 +15,20 @@ class WexModel extends CI_Model
 
 
 
-	public function getSession(){
+	public function getSession($session){
 
 		//return $this->db->query('SELECT CAST(data AS CHAR(10000) CHARACTER SET utf8) as data FROM ci_sessions')->result();
-		return $this->db->query('select * from ci_sessions')->result();
+		$this->db->select('*');
+		return $this->db->get_where('ci_sessions',"id = '".$session."'")->row();
 
 	}
+
+	public function getWexId($email){
+
+		$this->db->select('wex_id');
+		return $this->db->get_where("users", "email='".$email."'")->row();
+
+	}
+
 }
+
